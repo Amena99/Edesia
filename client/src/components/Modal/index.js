8e1a21f7c4835a0628a86548300d115a6df25c05
Amena Ahmed <am.ahmed09@gmail.com>
2019-04-14 18:53:00 -0500

    Added modal divs.

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -5,7 +5,25 @@ import "./style.css";
 // That way we don't have to define them all individually
 function Button (props) {
   return (
-    <button type={props.type} id={props.id} >{props.label}</button>
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            ...
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
