bb5c2d0ffd1e85367163d14c32f98cc2e96e7126
Amena Ahmed <am.ahmed09@gmail.com>
2019-04-17 13:22:36 -0500

    Created post method using FETCH API to tokenize purchase request and send to server.

--- client/src/components/CheckoutForm/index.js
+++ client/src/components/CheckoutForm/index.js
@@ -1,21 +1,45 @@
 import React, {Component} from 'react';
-import {CardElement, injectStripe} from 'react-stripe-elements';
+import {CardElement, CardNumberElement, CardExpiryElement, CardCVCElement, injectStripe} from 'react-stripe-elements';
 
 class CheckoutForm extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      complete: false
+    }
+
     this.submit = this.submit.bind(this);
   }
 
   async submit(ev) {
-    // User clicked submit
+    //tokenize the card information.
+    //we are invoking .createToken on the stripe prop. 
+    //stripe prop is available due to our wrapping it in injectStripe 
+    let {token} = await this.props.stripe.createToken({name: "Name"});
+
+    //send the token to the server.
+    //send it as POST request using broser FETCH API
+    let response = await fetch("/charge", {
+    method: "POST",
+    headers: {"Content-Type": "text/plain"},
+    body: token.id
+    });
+
+    if (response.ok) {this.setState({
+      complete: true
+    })}
   }
 
   render() {
     return (
       <div className="checkout">
         <p>Would you like to complete the purchase?</p>
-        <CardElement />
+        <p>Enter Credit Card Number:</p>
+        <CardNumberElement style={{base: {fontSize: '18px'}}}/>
+        <br></br>
+        <span>Expiration:</span><span><CardExpiryElement/></span><span>CVC:</span><span><CardCVCElement/></span>
+
         <button onClick={this.submit}>Send</button>
       </div>
     );
