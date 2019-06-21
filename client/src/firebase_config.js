f00734ea1604718ea3011d10feb04c7b06737c0d
Amena Ahmed <am.ahmed09@gmail.com>
2019-06-21 10:41:27 -0500

    Added firebase API Config and Firebase methods for sign in, create user, sign out, and password reset.

--- client/src/firebase_config.js
+++ client/src/firebase_config.js
@@ -1,4 +1,7 @@
-var firebaseConfig = {
+import app from "firebase/app";
+import "firebase/auth";
+
+const firebaseConfig = {
     apiKey: "AIzaSyAfQYd7gRQZvINPjh90HFszpogKeUrcY3w",
     authDomain: "edesia-1557464464178.firebaseapp.com",
     databaseURL: "https://edesia-1557464464178.firebaseio.com",
@@ -8,4 +11,21 @@ var firebaseConfig = {
     appId: "1:278603223557:web:2cdccf8851a45b23"
   };
 
-  export default firebaseConfig;
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+
+    this.auth = app.auth();
+  }
+
+  //defining authentication functions as class methods step by step
+  doCreateUserWithEmailAndPassword = (email, password) =>
+    this.auth.createUserWithEmailAndPassword(email, password);
+
+  doSignInWithEmailAndPassword = (email, password) => 
+    this.auth.signInWithEmailAndPassword(email, password);
+  
+  doSignOut = () => this.auth.signOut();
+}
+
+export default Firebase;
